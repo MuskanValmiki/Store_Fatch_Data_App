0544990ad885c345448d323ba16b2b68dddda6fc
Muskan Valmikee <muskanvalmikee@gmail.com>
2022-12-28 23:18:10 +0530

    second Natwest Project.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import {useState,useEffect} from 'react'
 import './App.css';
 
 function App() {
+  let [data_,setData] = useState([])
+  const API = `https://jsonplaceholder.typicode.com/users`
+  const getData=async ()=>{
+  const response = await fetch(API)
+    let data = await response.json()
+    setData(data)
+    // console.log(data)
+  } 
+  useEffect(()=>{
+    getData()
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table id='table'>
+        <tr>
+          <th id='th'>Id</th>
+          <th id='th'>Name</th>
+          <th id='th'>UserName</th>
+          <th id='th'>Email</th>
+          <th id='th'>Company Name</th>
+          <th id='th'>Zipcode</th>
+        </tr>
+      </table>
+      {
+        data_.map((item,index)=>{
+          return(
+            <div>
+              <table id='table'>
+                <tr>
+                    <td id='td'><h5>{item.id}</h5></td>
+                    <td id='td'><h5>{item.name}</h5></td>
+                    <td id='td'><h5>{item.username}</h5></td>
+                    <td id='td'><h5>{item.email}</h5></td>
+                    <td id='td'><h5>{item.company.name}</h5></td>
+                    <td id='td'><h5>{item.address.zipcode}</h5></td>
+                </tr>
+              </table>
+            </div>
+        )})
+      }
     </div>
-  );
+  )
 }
 
 export default App;
